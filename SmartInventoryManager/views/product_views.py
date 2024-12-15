22d540a2b866875cd46766ba3a18938c8e9e4bb1
Bar <bartalor@gmail.com>
2024-12-15 14:00:41 +0000

    add query params to product

--- SmartInventoryManager/views/product_views.py
+++ SmartInventoryManager/views/product_views.py
@@ -1,19 +1,32 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.request import Request
-from rest_framework.status import HTTP_400_BAD_REQUEST
 from SmartInventoryManager.models import Product
 from SmartInventoryManager.serializers import ProductSerializer
+from django.db.models import Q
 
 class ProductView(APIView):
-    def get(self, request):
-        products = Product.objects.all()
-        serializer = ProductSerializer(products, many=True)
-        return Response({'products': serializer.data})
+    def get(self, request: Request):
+        queryset = Product.objects.all()
 
-    def post(self, request: Request):
-        serializer = ProductSerializer(data=request.data)
-        if serializer.is_valid():
-            product = serializer.save()
-            return Response({"message": "Product created successfully", "id": product.id})
-        return Response(serializer.errors, status=HTTP_400_BAD_REQUEST)
+        category = request.query_params.get('category')
+        if category:
+            queryset = queryset.filter(category__name__icontains=category)
+
+        tags = request.query_params.getlist('tags')
+        if tags:
+            queryset = queryset.filter(tags__name__in=tags).distinct()
+
+        price_min = request.query_params.get('price_min')
+        price_max = request.query_params.get('price_max')
+        if price_min:
+            queryset = queryset.filter(price__gte=float(price_min))
+        if price_max:
+            queryset = queryset.filter(price__lte=float(price_max))
+
+        supplier = request.query_params.get('supplier')
+        if supplier:
+            queryset = queryset.filter(supplier__name__icontains=supplier)
+
+        serializer = ProductSerializer(queryset, many=True)
+        return Response({'products': serializer.data})
